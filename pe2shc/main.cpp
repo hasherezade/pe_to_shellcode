fe942d9b62e69dab903df258d158a572b76d89aa
hasherezade <hasherezade@gmail.com>
2018-12-15 19:49:37 +0100

    [REFACT] Changed the info strings

--- pe2shc/main.cpp
+++ pe2shc/main.cpp
@@ -35,7 +35,7 @@ BYTE* shellcodify(BYTE *my_exe, size_t exe_size, size_t &out_size, bool is64b)
 	int res_id = is64b ? STUB64 : STUB32;
 	BYTE *stub = peconv::load_resource_data(stub_size, res_id);
 	if (!stub) {
-		std::cout << "Stub not loaded" << std::endl;
+		std::cout << "[-] Stub not loaded" << std::endl;
 		return nullptr;
 	}
 	size_t ext_size = exe_size + stub_size;
@@ -57,7 +57,7 @@ bool is_supported_pe(BYTE *my_exe, size_t exe_size)
 {
 	if (!my_exe) return false;
 	if (!peconv::has_relocations(my_exe)) {
-		std::cout << "The PE must have relocations!" << std::endl;
+		std::cout << "[-] The PE must have relocations!" << std::endl;
 		return false;
 	}
 	if (peconv::get_subsystem(my_exe) != IMAGE_SUBSYSTEM_WINDOWS_GUI) {
@@ -65,12 +65,12 @@ bool is_supported_pe(BYTE *my_exe, size_t exe_size)
 	}
 	IMAGE_DATA_DIRECTORY* dotnet_dir = peconv::get_directory_entry(my_exe, IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR);
 	if (dotnet_dir) {
-		std::cout << ".NET applications are not supported!" << std::endl;
+		std::cout << "[-] .NET applications are not supported!" << std::endl;
 		return false;
 	}
 	IMAGE_DATA_DIRECTORY* tls_dir = peconv::get_directory_entry(my_exe, IMAGE_DIRECTORY_ENTRY_TLS);
 	if (tls_dir) {
-		std::cout << "Applications with TLS callbacks are not supported!" << std::endl;
+		std::cout << "[-] Applications with TLS callbacks are not supported!" << std::endl;
 		return false;
 	}
 	return true;
@@ -104,7 +104,7 @@ int main(int argc, char *argv[])
 	std::cout << "Reading module from: " << in_path << std::endl;
 	BYTE *my_exe = peconv::load_file(in_path.c_str(), exe_size);
 	if (!my_exe) {
-		system("pause");
+		std::cout << "[-] Could not read the input file!" << std::endl;
 		return -1;
 	}
 	if (!is_supported_pe(my_exe, exe_size)) {
@@ -121,7 +121,7 @@ int main(int argc, char *argv[])
 		return -3;
 	}
 	if (peconv::dump_to_file(out_str.c_str(), ext_buf, ext_size)) {
-		std::cout << "[+] Saved to file: " << out_str << std::endl;
+		std::cout << "[+] Saved as: " << out_str << std::endl;
 	}
 	else {
 		std::cout << "[-] Failed to save the output!" << std::endl;
