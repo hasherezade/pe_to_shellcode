dfd23ea6f01290b9ee09a8fc2bdb49b13f7ae3c7
hasherezade <hasherezade@gmail.com>
2021-02-02 04:11:48 +0100

    [FEATURE][tests] Make DPC test display its own PID

--- tests/dpc_test/main.cpp
+++ tests/dpc_test/main.cpp
@@ -6,7 +6,7 @@ int main()
 	PROCESS_MITIGATION_DYNAMIC_CODE_POLICY dcp = {};
 	dcp.ProhibitDynamicCode = 1;
 	SetProcessMitigationPolicy(ProcessDynamicCodePolicy, &dcp, sizeof(dcp));
-
+	std::cout << "PID: " << std::dec << GetCurrentProcessId() << "\n";
 	std::cout << "PROCESS_MITIGATION_DYNAMIC_CODE_POLICY enabled...\n";
 	while (true)
 	{
