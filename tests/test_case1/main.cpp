2caf032d4c020d06da789dad46403ed87495f14d
hasherezade <hasherezade@gmail.com>
2021-02-02 09:06:23 +0100

    [BUGFIX] Fixed a testcase

--- tests/test_case1/main.cpp
+++ tests/test_case1/main.cpp
@@ -1,6 +1,8 @@
 #include <Windows.h>
 #include <iostream>
 
+//#define WITH_MESSAGE //only for manual tests
+
 int get_date()
 {
     SYSTEMTIME SystemTime;
@@ -17,7 +19,9 @@ int main()
     if (get_date() == 1337) {
         std::cout << "Test passed!\n";
     }
+#ifdef WITH_MESSAGE
     MessageBoxW(0, L"Hello World!", L"Demo!", MB_OK);
+#endif
     std::cout << "Test Case 1 finished\n";
     return 0;
 }
