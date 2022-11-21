9f987b598d1c457f0798e59d13459530c67911e0
hasherezade <hasherezade@gmail.com>
2022-11-21 09:40:46 -0800

    [FEATURE] Run module in new thread. Allow to unload DLL

--- runshc/main.cpp
+++ runshc/main.cpp
@@ -3,6 +3,88 @@
 
 #include <peconv.h>
 
+typedef struct {
+	BYTE* my_exe;
+	size_t exe_size;
+	bool is_run;
+} t_module_params;
+
+bool load_and_run(t_module_params& args)
+{
+	BYTE* test_buf = peconv::alloc_aligned(args.exe_size, PAGE_EXECUTE_READWRITE);
+	if (!test_buf) {
+		std::cerr << "[ERROR] Allocating buffer failed" << std::endl;
+		return false;
+	}
+
+	//copy file content into executable buffer:
+	memcpy(test_buf, args.my_exe, args.exe_size);
+
+	//free the original buffer:
+	peconv::free_file(args.my_exe);
+	args.my_exe = nullptr;
+
+	std::cout << "[*] Running the shellcode:" << std::hex << (ULONG_PTR) test_buf << " to: " << (ULONG_PTR)(test_buf + args.exe_size)<<  std::endl;
+	//run it:
+	int (*my_main)() = (int(*)()) ((ULONGLONG)test_buf);
+	int ret_val = my_main();
+	args.is_run = true;
+	if (peconv::is_module_dll(test_buf)) {
+		//unload DLL:
+		DWORD ep_rva = peconv::get_entry_point_rva(test_buf);
+		std::cout << "Unload DLL, RVA: " << ep_rva << "\n";
+		ULONG_PTR ep_va = (ULONG_PTR)test_buf + ep_rva;
+		BOOL(WINAPI * my_DllMain)(HINSTANCE, DWORD, LPVOID)
+			= (BOOL(WINAPI*)(HINSTANCE, DWORD, LPVOID)) ep_va;
+		my_DllMain((HINSTANCE)test_buf, DLL_PROCESS_DETACH, 0);
+	}
+	peconv::free_aligned(test_buf, args.exe_size);
+	std::cout << "[+] The shellcode finished with a return value: " << std::hex << ret_val << std::endl;
+	return true;
+}
+
+
+DWORD WINAPI mod_runner(LPVOID lpParam)
+{
+	t_module_params* args = static_cast<t_module_params*>(lpParam);
+	if (!args) {
+		return ERROR_BAD_ARGUMENTS;
+	}
+	args->is_run = false;
+	load_and_run(*args);
+	return S_OK;
+}
+
+bool run_in_new_thread(t_module_params &args)
+{
+	std::cout << __FUNCTION__ << std::endl;
+
+	std::cout << ">>> Creating the thread...\n";
+	HANDLE hThead = CreateThread(
+		NULL,                   // default security attributes
+		0,                      // use default stack size  
+		mod_runner,       // thread function name
+		&args,          // argument to thread function 
+		0,                      // use default creation flags 
+		0);   // returns the thread identifier 
+
+	if (!hThead) {
+		std::cerr << "Failed to created the thread!\n";
+		return false;
+	}
+	DWORD wait_result = WaitForSingleObject(hThead, INFINITE);
+	return (args.is_run);
+}
+
+bool run_in_curr_thread(t_module_params &args)
+{
+	std::cout << __FUNCTION__ << std::endl;
+	load_and_run(args);
+	return (args.is_run);
+}
+
+#define NEW_THREAD
+
 int main(int argc, char *argv[])
 {
 	if (argc < 2) {
@@ -43,26 +125,21 @@ int main(int argc, char *argv[])
 #endif
 	}
 
-	BYTE *test_buf = peconv::alloc_aligned(exe_size, PAGE_EXECUTE_READWRITE);
-	if (!test_buf) {
-		peconv::free_file(my_exe);
-		std::cerr << "[ERROR] Allocating buffer failed" << std::endl;
-		return -2;
+	t_module_params args = { 0 };
+	args.my_exe = my_exe;
+	args.exe_size = exe_size;
+	args.is_run = false;
+
+#ifdef NEW_THREAD
+	bool res = run_in_new_thread(args);
+#else
+	bool res = run_in_curr_thread(args);
+#endif
+	if (args.my_exe) {
+		peconv::free_file(args.my_exe);
+		args.my_exe = nullptr;
+		my_exe = nullptr;
 	}
-
-	//copy file content into executable buffer:
-	memcpy(test_buf, my_exe, exe_size);
-
-	//free the original buffer:
-	peconv::free_file(my_exe);
-	my_exe = nullptr;
-
-	std::cout << "[*] Running the shellcode:" << std::endl;
-	//run it:
-	int (*my_main)() = (int(*)()) ((ULONGLONG)test_buf);
-	int ret_val = my_main();
-	
-	peconv::free_aligned(test_buf, exe_size);
-	std::cout << "[+] The shellcode finished with a return value: " << std::hex << ret_val << std::endl;
-	return ret_val;
+	std::cout << ">>> FINISHED.\n";
+	return 0;
 }
